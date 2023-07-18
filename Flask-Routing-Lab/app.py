e91a2df68dd361680d98751e272150cf85f3cfe1
talda24-meet <139680993+talda24-meet@users.noreply.github.com>
2023-07-18 17:25:42 +0300

    Finished flast lab

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -9,10 +9,18 @@ app = Flask(  # Create a flask app
 
 # Your code should be below
 
+@app.route('/')
+def home_page():
+    return render_template("home.html")
 
-
-
+@app.route('/product')
+def product_page():
+    return render_template("product.html")
 # Your code should be above
+
+@app.route('/cart')
+def cart_page():
+    return render_template("cart.html")
 
 if __name__ == "__main__":  # Makes sure this is the main process
     app.run(debug=True)
